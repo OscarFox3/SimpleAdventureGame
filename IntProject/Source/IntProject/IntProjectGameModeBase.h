90f9ec7e9a83c24897554f7a76628e3048f64268
Oscar Fox <73188740+OscarFox3@users.noreply.github.com>
2021-10-21 18:09:15 -0400

    First Commit

---
+++ IntProject/Source/IntProject/IntProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "IntProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTPROJECT_API AIntProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
