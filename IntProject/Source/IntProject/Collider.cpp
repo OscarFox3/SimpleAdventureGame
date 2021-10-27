5cdf90ad9c98666b9d088b7e27063044bb0cf46b
Oscar Fox <73188740+OscarFox3@users.noreply.github.com>
2021-10-26 22:14:47 -0400

    Pawn Movement Input

--- IntProject/Source/IntProject/Collider.cpp
+++ IntProject/Source/IntProject/Collider.cpp
@@ -2,12 +2,53 @@
 
 
 #include "Collider.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
 
 // Sets default values
 ACollider::ACollider()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+	SphereComponent->SetupAttachment(GetRootComponent());
+	SphereComponent->InitSphereRadius(40.f);
+	SphereComponent->SetCollisionProfileName(TEXT("Pawn"));
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	MeshComponent->SetupAttachment(GetRootComponent());
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> MeshComponentAsset(TEXT("StaticMesh'/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere'"));
+	
+	// If ConstructorHelpers::ObjectFinder finds a static mesh at the path
+	if (MeshComponentAsset.Succeeded())
+	{
+		// Member variable Object of StaticMesh is set to MeshComponent
+		MeshComponent->SetStaticMesh(MeshComponentAsset.Object);
+		// MeshComponent is centered with respect to collison sphere component
+		MeshComponent->SetRelativeLocation(FVector(0.f, 0.f, -40.f));
+		MeshComponent->SetWorldScale3D(FVector(0.8f, 0.8f, 0.8f));
+	}
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(GetRootComponent());
+	SpringArm->SetRelativeRotation(FRotator(-45.f, 0.f, 0.f));
+	SpringArm->TargetArmLength = 400.f;
+	SpringArm->bEnableCameraLag = true;
+	SpringArm->CameraLagSpeed = 3.0f;
+
+	// create camera
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
 
 }
 
@@ -30,5 +71,19 @@ void ACollider::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis(TEXT("MoveFoward"), this, &ACollider::MoveFoward);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &ACollider::MoveFoward);
+
 }
 
+void ACollider::MoveFoward(float input)
+{
+	FVector Forward = GetActorForwardVector();
+	AddMovementInput(input * Forward);
+}
+
+void ACollider::MoveRight(float input)
+{
+	FVector Right = GetActorRightVector();
+	AddMovementInput(input * Right);
+}
--- IntProject/Source/IntProject/Collider.h
+++ IntProject/Source/IntProject/Collider.h
@@ -26,4 +26,33 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UPROPERTY(VisibleAnywhere, Category = "Mesh")
+	class UStaticMeshComponent* MeshComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Mesh")
+	class USphereComponent* SphereComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Mesh")
+	class UCameraComponent* Camera;
+
+	UPROPERTY(VisibleAnywhere, Category = "Mesh")
+	// control camera motion
+	class USpringArmComponent* SpringArm;
+
+	// inline functions
+	FORCEINLINE UStaticMeshComponent* GetMeshComponent() { return MeshComponent; }
+	FORCEINLINE void SetMeshComponent(UStaticMeshComponent* Mesh) { MeshComponent = Mesh; }
+	FORCEINLINE USphereComponent* GetSphereComponent() { return SphereComponent; }
+	FORCEINLINE void SetSphereComponent(USphereComponent* Sphere) { SphereComponent = Sphere; }
+
+	FORCEINLINE UCameraComponent* GetCameraComponent() { return Camera; }
+	FORCEINLINE void SetCameraComponent(UCameraComponent* InCamera) { Camera = InCamera; }
+	FORCEINLINE USpringArmComponent* GetSpringArmComponent() { return SpringArm; }
+	FORCEINLINE void SetSpringArmComponent(USpringArmComponent* InSpringArm) { SpringArm = InSpringArm; }
+
+private:
+
+	void MoveFoward(float input);
+	void MoveRight(float input);
+
 };
