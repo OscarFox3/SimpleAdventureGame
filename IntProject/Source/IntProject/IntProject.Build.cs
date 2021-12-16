e688971944afaa9ea28888fce2f3304adfbdcc41
Oscar Fox <73188740+OscarFox3@users.noreply.github.com>
2021-12-15 19:36:40 -0500

    Prep Package

--- IntProject/Source/IntProject/IntProject.Build.cs
+++ IntProject/Source/IntProject/IntProject.Build.cs
@@ -8,7 +8,7 @@ public class IntProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "AIModule" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "AIModule", "ApplicationCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
